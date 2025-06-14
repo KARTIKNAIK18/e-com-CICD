14283ed1160ac58412ba19277c23ad1947a3e200
KARTIKNAIK18 <kartiknaik560@gmail.com>
2025-06-14 12:52:14 +0530

    K8S file added

---
+++ k8s/kube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+touch ecom-backend-deployment.yml
+touch ecom-frontend-deployment.yml
+touch ecom-frontend-service.yml
+touch ecom-backend-service.yml
+touch ecom-ingress.yml
+
+echo "all the files created succefully"
